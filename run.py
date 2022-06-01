8bc74ef6bbaacf5bac73214520dc06042a4e3988
erikandersson96 <erik96.andersson@gmail.com>
2022-06-01 10:00:19 +0000

    Add page_title to run.py to match each index

--- run.py
+++ run.py
@@ -6,26 +6,26 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index(): 
+def index():
     return render_template("index.html")
 
 
 @app.route("/about")
-def about(): 
-    return render_template("about.html")
+def about():
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
-def contact(): 
-    return render_template("contact.html")
+def contact():
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
-def careers(): 
-    return render_template("careers.html")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
